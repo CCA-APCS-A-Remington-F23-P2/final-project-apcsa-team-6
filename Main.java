e660c55d6f3f25d84c1dca54882273c4a5da98e7
sarahsarahw <152912894+sarahsarahw@users.noreply.github.com>
2023-12-06 19:21:10 +0000

    good

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args){
       System.out.println("HI!");
 
-      // :) hallo how re you
+      // :) hallo how re you im good thnk u
     }
   
 }
