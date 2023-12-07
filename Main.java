284a87e7024eb553c7f9fddcccf4b06736d03679
jahnavi-rs <152912851+jahnavi-rs@users.noreply.github.com>
2023-12-07 18:43:08 +0000

    added !

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
   
     public static void main(String[] args){
-      System.out.println("HI!");
+      System.out.println("HI!!!");
 
       // :) hallo how re you im good thnk u
     }
