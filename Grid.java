5eff2003e6b09ce825ddab4a7a9e26140f50e89f
zhangd8412 <152912751+zhangd8412@users.noreply.github.com>
2023-12-08 19:37:55 +0000

    update gird

--- Grid.java
+++ Grid.java
@@ -9,28 +9,65 @@ import java.util.List;
 public class Grid
 {
   private List<Hole> holes;
+  private List <Mole> moles;
+  private List <Bomb> bombs;
   int size;
 
   public Grid(int size)
   {
     holes = new ArrayList<Hole>();
+    moles = new ArrayList<Mole>();
+    bombs = new ArrayList<Bomb>();
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < size; j++) {
-        add(new Hole(100 + j * 85, 100 + i * 85, 80, 80));
+        holes.add(new Hole(100 + j * 85, 100 + i * 85, 80, 80));
       }
     }
     this.size = size;
   }
 
-  public void add(Hole hole)
+  public List<Mole> getMoles()
   {
-    holes.add(hole);
+    return moles;
+  }
+
+  public List<Bomb> getBombs()
+  {
+    return bombs;
+  }
+
+  public void addMole()
+  {
+    int rand = (int) (Math.random() * holes.size());
+    moles.add(new Mole(holes.get(rand).getX(), holes.get(rand).getY(), 80, 80));
+  }
+
+  public void removeMole(int i)
+  {
+    moles.remove(i);
+  }
+
+  public void addBomb()
+  {
+    int rand = (int)(Math.random() * holes.size());
+    bombs.add(new Bomb(holes.get(rand).getX(), holes.get(rand).getY(), 80, 80));
+  }
+
+  public void removeBomb(int i)
+  {
+    bombs.remove(i);
   }
 
   public void draw(Graphics window)
   {
     for (Hole h : holes) {
       h.draw(window);
+    }
+    for (Mole m : moles) {
+      m.draw(window);
+    }
+    for (Bomb b : bombs) {
+      b.draw(window);
     }
   }
 }
